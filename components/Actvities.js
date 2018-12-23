23191d63426250c8b3f4cf086f6c496734f222e2
Stefanos <stefanosu422@gmail.com>
2018-12-23 18:39:26 -0500

    Added new file

---
+++ components/Actvities.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class ActivityItem extends Component{
+	render() {
+		const{activity}= this.props; //ES6 destructuring 
+
+		return(
+			<div className="item">
+			<div className="avatar">
+			  <img 
+			  	alt={activity.text}
+			  	src={activity.user.avatar} />
+			  {activity.user.name}
+			  </div> 
+
+			<span className="time">
+				{activity.timestamp}
+				</span>
+			<p>{activity.text}</p>
+			<div className="commentCount">
+				{activity.comments.length}
+			</div> 		 
+			</div>
+
+		)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
